57c61d41e84c722f4c7bc8dfd76c298a445aced3
MS0l3 <msole.1@alum.joviat.cat>
2025-04-01 12:48:58 +0200

    primerprograma

---
+++ src/provajava1/prova1.java
@@ -0,0 +1,13 @@
+package provajava1;
+
+public class prova1 {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo!");
+        
+        int numero1 = 5;
+        int numero2 = 10;
+        int suma = numero1 + numero2;
+        
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
