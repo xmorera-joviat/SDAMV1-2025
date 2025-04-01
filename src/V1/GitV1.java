cbdb2fbbece54cbc5e698def48d3aa7c6851e002
TheFalcon98 <thefalcon981@gmail.com>
2025-04-01 12:56:15 +0200

    Programma 2 Raúl

--- src/V1/GitV1.java
+++ src/V1/GitV1.java
@@ -1,19 +1,30 @@
 package V1;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GitV1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Introdueix un número: ");
-        int num = scanner.nextInt();
-        long factorial = 1;
+        Random random = new Random();
+        int numeroSecret = random.nextInt(100) + 1;
+        int intent;
         
-        for (int i = 1; i <= num; i++) {
-            factorial *= i;
-        }
+        System.out.println("Benvingut al joc! Intenta endevinar el número (entre 1 i 100).");
         
-        System.out.println("El factorial de " + num + " és: " + factorial);
+        do {
+            System.out.print("Introdueix la teva endevinalla: ");
+            intent = scanner.nextInt();
+            
+            if (intent < numeroSecret) {
+                System.out.println("Més alt!");
+            } else if (intent > numeroSecret) {
+                System.out.println("Més baix!");
+            } else {
+                System.out.println("Felicitats! Has endevinat el número.");
+            }
+        } while (intent != numeroSecret);
+        
         scanner.close();
     }
 }
