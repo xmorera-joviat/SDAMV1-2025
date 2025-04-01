c828ce819ee31940edc061f9b84755b77ac3e97b
MS0l3 <msole.1@alum.joviat.cat>
2025-04-01 12:56:40 +0200

    segonaversio

--- src/provajava1/prova1.java
+++ src/provajava1/prova1.java
@@ -1,13 +1,8 @@
 package provajava1;
 
 public class prova1 {
-    public static void main(String[] args) {
-        System.out.println("Hola, mundo!");
-        
-        int numero1 = 5;
-        int numero2 = 10;
-        int suma = numero1 + numero2;
-        
-        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+	public static void main(String[] args) {
+        String nombre = "Usuario";
+        System.out.println("Hola, " + nombre + "! Bienvenido a Java.");
     }
 }
